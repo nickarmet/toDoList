3d95beb851b42f2111bb49ed99bc703fff512f02
Nick Armet <nick.armet@gmail.com>
2018-06-12 13:18:17 -0500

    v2 in progress

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+//Check off specific to-do's by clicking
+$("ul").on("click", "li", function() {
+	$(this).toggleClass("completed");
+});
+//Click on X to delete todo
+$("ul").on("click", "span", function(event) {
+	$(this).parent().fadeOut(500, function() {
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+$("input[type='text']").on("keypress", function(event) {
+	if(event.which === 13) {
+		let toDoText = $(this).val();
+		$("ul").append(`<li><span><i class='fas fa-trash-alt'></i></span> ${toDoText}</li>`);
+		$(this).val("");
+	}
+})
